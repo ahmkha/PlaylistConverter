a850629fb718adee78c31073e4903b63a5b9153d
ahmkhan <ahmed.khan@stonybrook.edu>
2021-01-15 20:40:20 -0500

    Playlist selection display done.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, make_response
 sys.path.insert(1, 'serverCode')
 from serverCode import spotifyAuth
 
 app = Flask(__name__)
+spotify_access_token = ''
 
 @app.route('/')
 def index():
@@ -19,11 +20,25 @@ def ytToSpotify():
 
 @app.route('/SpotifyLogin')
 def spotifyLogin():
-    return redirect(spotifyAuth.auth(1, 1))
+    return redirect(spotifyAuth.auth())
 
 @app.route('/SpotifyCallback')
 def spotifyCallback():
-    return "Succesfully logged in!"
+    codeParam = request.args.get('code')
+    # Also check if state has changed as well later
+    if codeParam == None:
+        return "Bro, you're buggin..."
+    authedPage = spotifyAuth.callback(str(codeParam))
+    # return authedPage.json()
+    global spotify_access_token 
+    spotify_access_token = authedPage.json()['access_token']
+    return redirect('/SpotifyAuthed')
+
+@app.route('/SpotifyAuthed')
+def spotifyAuthed():
+    global spotify_access_token
+    currentUserPlaylists = spotifyAuth.getCurrentUser(spotify_access_token).json()['items']
+    return render_template('spotifyPlaylistView.html', playlists=currentUserPlaylists)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- serverCode/spotifyAuth.py
+++ serverCode/spotifyAuth.py
@@ -4,18 +4,25 @@ import os
 from apiKeys import *
 
 clientId = spotifyClientID
-redirectURI = 'http://localhost:5000/SpotifyCallback'
+secretKey = spotifyClientSecret
+authRedirectURI = 'http://localhost:5000/SpotifyCallback'
 
 
-def auth(request, response):
+def auth():
     scope = 'user-library-read'
-    # state = os.urandom(16)
-    queryStringDict = { 'client_id': clientId, 'response_type': 'token', 'redirect_uri': redirectURI, 'scope': scope, 'state': "123456789012345"}
+    # state = os.urandom(16) # will change to actual random 16 character state
+    queryStringDict = { 'client_id': clientId, 'response_type': 'code', 'redirect_uri': authRedirectURI, 'scope': scope, 'state': "123456789012345"}
     queryString = urlencode(queryStringDict)
 
-    # response = requests.get('https://accounts.spotify.com/authorize?' + queryString)
     response = 'https://accounts.spotify.com/authorize?' + queryString
     return response
 
-def callback(request, response):
-    return None
+def callback(code):
+    response = requests.post('https://accounts.spotify.com/api/token', \
+        data={'grant_type': 'authorization_code', 'code': code, 'redirect_uri': authRedirectURI, 'client_id': clientId, 'client_secret': secretKey})
+    return response
+
+def getCurrentUser(accessToken):
+    headers = {'Authorization': 'Bearer ' + str(accessToken)}
+    response = requests.get('https://api.spotify.com/v1/me/playlists', headers = headers)
+    return response
